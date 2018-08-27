139e436fd9015f96522f5c3b8bb70890dc247f9a
“Tyler <tylerpalef@gmail.com>
2018-08-27 13:59:03 -0400

    created initial 3 tests for the default initialize

--- rocket_test.rb
+++ rocket_test.rb
@@ -2,5 +2,36 @@ require "minitest/autorun"
 require_relative "rocket"
 
 class RocketTest < Minitest::Test
-  # Write your tests here!
+  def setup
+    @rocket = Rocket.new
+  end
+
+def test_initalize_default_name
+  # arrange
+  # act
+  expected = @rocket.name
+  actual = @rocket.name
+  #assert
+  assert_equal(expected, actual)
+end
+
+def test_initalize_default_colour
+  # arrange
+  # act
+  expected = @rocket.colour
+  actual = @rocket.colour
+  #assert
+  assert_equal(expected, actual)
+end
+
+def test_initalize_default_flying
+  # arrange
+  # act
+  expected = @rocket.flying?
+  actual = false
+  #assert
+  assert_equal(expected, actual)
+end
+
+
 end
